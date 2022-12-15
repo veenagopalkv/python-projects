9d6867e7330aea9063fee9b7a267c8606dcaff8b
veenagopalkv <veenagopalkv@gmail.com>
2022-12-15 15:44:51 +0530

    Delete travelproject directory

--- travelproject/travelproject/travelapp/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .  models import place
-from . models import people
-
-# Register your models here.
-admin.site.register(place)
-admin.site.register(people)
-
-
--- travelproject/travelproject/travelapp/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from . models import place
-from . models import people
-
-# Create your views here.
-def demo(request):
-    obj=place.objects.all()
-    peopleinfo=people.objects.all()
-    return render(request,'index.html',{'result':obj,'pepinfo':peopleinfo})
